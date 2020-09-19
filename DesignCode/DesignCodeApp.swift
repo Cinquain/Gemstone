c50081f9c7d641b3c343bb0d7fcae8d46c479672
James Allan <James@cityXcape.com>
2020-09-18 21:20:58 -0400

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by James Allan on 9/18/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
